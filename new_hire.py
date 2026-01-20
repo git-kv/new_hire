7329ff00bfaf3f369306dc677e69fa675bbef11c
Keith <8584184+git-kv@users.noreply.github.com>
2026-01-20 08:52:02 -0600

    added imports and logging setup

---
+++ new_hire.py
@@ -0,0 +1,10 @@
+import os, sys, logging, csv
+import pandas as pd
+
+from datetime import datetime, timedelta, time
+from PyQt6.QtCore import Qt, QCoreApplication, QDate
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit, QVBoxLayout, QWidget, QLineEdit, QPushButton, QSpacerItem, QCalendarWidget, QCheckBox, QMessageBox
+
+today = datetime.today().strftime('%Y-%m-%d')
+log_path = '\\\\eocservices\\apps$\\programs\\scripts\\logs\\employee_termination_' + today + '.log'
+logging.basicConfig(filename=log_path, level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
