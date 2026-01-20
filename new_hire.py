122b803fa3f2cb44f8007e5ab98c7869901d40e5
Keith <8584184+git-kv@users.noreply.github.com>
2026-01-20 11:59:05 -0600

    updated logfile name

--- new_hire.py
+++ new_hire.py
@@ -6,5 +6,5 @@ from PyQt6.QtCore import Qt, QCoreApplication, QDate
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit, QVBoxLayout, QWidget, QLineEdit, QPushButton, QSpacerItem, QCalendarWidget, QCheckBox, QMessageBox
 
 today = datetime.today().strftime('%Y-%m-%d')
-log_path = '\\\\eocservices\\apps$\\programs\\scripts\\logs\\employee_termination_' + today + '.log'
+log_path = '\\\\eocservices\\apps$\\programs\\scripts\\logs\\employee_account_creation_' + today + '.log'
 logging.basicConfig(filename=log_path, level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
